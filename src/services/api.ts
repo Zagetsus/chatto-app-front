4bd32ab338469a59827b77190604d603dd539e6b
Luan Verdelho <luan@houpa.app>
2021-11-03 14:39:19 -0300

    Aceitando api sem o ssl

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,14 @@
 import axios from "axios";
+import * as https from "https";
+
+const httpsAgent = new https.Agent({ rejectUnauthorized: false });
 
 export const api = axios.create({
-    baseURL: process.env.REACT_APP_API_BASEURL
+    baseURL: process.env.REACT_APP_API_BASEURL,
+    httpsAgent,
+    headers: {
+        'Accept-Language': 'es-ES,es;q=0.8',
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Accept": "application/json",
+    }
 })
